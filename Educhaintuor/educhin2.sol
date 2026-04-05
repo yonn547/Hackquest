44d3d90a69771fb90fbb80b86543322052b90024
yonn <meh535121@gmail.com>
2026-04-05 11:53:32 +0700

    buatnfteduchain

--- Educhaintuor/educhin2.sol
+++ Educhaintuor/educhin2.sol
@@ -34,11 +34,52 @@ contract HackQuest is ERC721, ERC721URIStorage, Ownable {
         _signer = signer;
     }
 
+
     function safeMint() public {
         uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, ipfsUris[0]);
         _courseProgress[tokenId] = CourseProgress.INIT;
+    }
+
+    function updateCourseProgress(uint256 tokenId, uint8 progress, bytes memory signature) public {
+        // verify signature
+        require(!_signatures[signature], "Signature Already Used");
+        bytes32 _msgHash = getMessageHash(msg.sender, tokenId, progress);
+        bytes32 _ethSignedMessageHash = toEthSignedMessageHash(_msgHash);
+        require(verify(_ethSignedMessageHash, signature), "Invalid Signature");
+        _signatures[signature] = true;
+
+        // verify tokenId
+        address from = _ownerOf(tokenId);
+        require(from == msg.sender, "Invalid TokenId");
+
+        // verify course progress
+        require(_courseProgress[tokenId] == CourseProgress.INIT || _courseProgress[tokenId] == CourseProgress.PROGRESS_HALF, "Course Progress Error");
+
+        if (progress == uint8(CourseProgress.PROGRESS_HALF)) {
+            _courseProgress[tokenId] = CourseProgress.PROGRESS_HALF;
+            _setTokenURI(tokenId, ipfsUris[1]);
+        } else if (progress == uint8(CourseProgress.PROGRESS_COMPLETE)) {
+            _courseProgress[tokenId] = CourseProgress.PROGRESS_COMPLETE;
+            _setTokenURI(tokenId, ipfsUris[2]);
+        }
+    }
+
+    
+    function getMessageHash(address _account, uint256 _tokenId, uint8 _progress) public pure returns(bytes32) {
+        return keccak256(abi.encodePacked(_account, _tokenId, _progress));
+    }
+    
+    function toEthSignedMessageHash(bytes32 hash) internal pure returns (bytes32) {
+        // 32 is the length in bytes of hash,
+        // enforced by the type signature above
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    }
+
+    function verify(bytes32 _msgHash, bytes memory _signature) public view returns (bool) {
+        (address recovered,,) = ECDSA.tryRecover(_msgHash, _signature);
+        return recovered == _signer;
     }
 
     // The following functions are overrides required by Solidity.
